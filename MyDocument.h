9a9e0477bc1f533aa060ed958f70f8c79e1c5371
Oleg Andreev <oleganza@gmail.com>
2010-05-03 22:54:23 +0200

    init

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  gitbox
+//
+//  Created by Oleg Andreev on 5/3/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  gitbox
+//
+//  Created by Oleg Andreev on 5/3/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
