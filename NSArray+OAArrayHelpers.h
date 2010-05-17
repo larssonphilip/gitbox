f290d4181a9655091884604156c43d24f0838e4d
Oleg Andreev <oleganza@gmail.com>
2010-05-17 17:32:37 +0200

    array helpers

--- NSArray+OAArrayHelpers.h
+++ NSArray+OAArrayHelpers.h
@@ -1,5 +1,11 @@
 @interface NSArray (OAArrayHelpers)
 
 - (id) firstObject;
+- (NSArray*) reversedArray;
+- (id) objectAtIndex:(NSUInteger)index or:(id)defaultObject;
 
+@end
+
+@interface NSMutableArray (OAArrayHelpers)
+- (NSMutableArray*) reverse;
 @end
--- NSArray+OAArrayHelpers.m
+++ NSArray+OAArrayHelpers.m
@@ -4,7 +4,44 @@
 
 - (id) firstObject
 {
-  return [self objectAtIndex:0];
+  return [self objectAtIndex:0 or:nil];
+}
+
+- (NSArray*) reversedArray
+{
+  NSMutableArray* array = [NSMutableArray arrayWithCapacity:[self count]];
+  NSEnumerator* enumerator = [self reverseObjectEnumerator];
+  for (id element in enumerator)
+  {
+    [array addObject:element];
+  }
+  return array;
+}
+
+- (id) objectAtIndex:(NSUInteger)index or:(id)defaultObject
+{
+  if (index >= [self count]) return defaultObject;
+  id r = [self objectAtIndex:index];
+  if (!r) return defaultObject;
+  return r;
+}
+
+@end
+
+
+@implementation NSMutableArray (OAArrayHelpers)
+
+- (NSMutableArray*) reverse
+{
+  NSUInteger i = 0;
+  NSUInteger j = [self count] - 1;
+  while (i < j)
+  {
+    [self exchangeObjectAtIndex:i withObjectAtIndex:j];
+    i++;
+    j--;
+  }
+  return self;
 }
 
 @end
