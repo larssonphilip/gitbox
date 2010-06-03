17f8abaced59bae888df8c1bbde8854e5b5a4953
Oleg Andreev <oleganza@gmail.com>
2010-06-03 10:15:12 +0200

    mega cool tooltip

--- GBCommitPromptController.h
+++ GBCommitPromptController.h
@@ -1,9 +1,14 @@
-@interface GBCommitPromptController : NSWindowController<NSWindowDelegate>
+@interface GBCommitPromptController : NSWindowController<NSWindowDelegate, NSTextViewDelegate>
 {
+  BOOL addedNewLine;
+  BOOL removedNewLine;
+  BOOL finishedPlayingWithTooltip;
 }
 @property(retain) NSString* value;
 
 @property(retain) IBOutlet NSTextView* textView;
+@property(retain) IBOutlet NSTextField* shortcutTipLabel;
+
 @property(assign) id target;
 @property(assign) SEL finishSelector;
 @property(assign) SEL cancelSelector;
--- GBCommitPromptController.m
+++ GBCommitPromptController.m
@@ -7,6 +7,8 @@
 
 @synthesize value;
 @synthesize textView;
+@synthesize shortcutTipLabel;
+
 @synthesize target;
 @synthesize finishSelector;
 @synthesize cancelSelector;
@@ -19,13 +21,25 @@
 
 - (void) dealloc
 {
+  [NSObject cancelPreviousPerformRequestsWithTarget:self];
   self.value = nil;
   self.textView = nil;
+  self.shortcutTipLabel = nil;
   [super dealloc];
+}
+
+- (void) resetMagicFlags
+{
+  [self.shortcutTipLabel setHidden:YES];
+  [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(showShortcutTip) object:nil];
+  addedNewLine = NO;
+  removedNewLine = NO;
+  finishedPlayingWithTooltip = NO;
 }
 
 - (IBAction) onOK:(id)sender
 {
+  [self resetMagicFlags];
   self.value = [self.textView string];
   
   if (!self.value || [self.value isEmptyString]) return;
@@ -42,6 +56,7 @@
 
 - (IBAction) onCancel:(id)sender
 {
+  [self resetMagicFlags];
   if (cancelSelector) [self.target performSelector:cancelSelector withObject:self];
   [self.windowHoldingSheet endSheetForController:self];
   self.windowHoldingSheet = nil;
@@ -58,11 +73,59 @@
 #pragma mark NSWindowDelegate
 
 
-- (void) windowDidBecomeKey:(NSNotification *)notification
+- (void) windowDidBecomeKey:(NSNotification*)notification
 {
   [self.textView selectAll:self];
 }
 
+
+
+#pragma mark NSTextViewDelegate
+
+
+- (BOOL)textView:(NSTextView*)aTextView
+        shouldChangeTextInRange:(NSRange)affectedCharRange
+        replacementString:(NSString*)replacementString
+{
+  if (!finishedPlayingWithTooltip)
+  {
+    if (!addedNewLine)
+    {
+      if (affectedCharRange.location == [[aTextView string] length] && 
+          affectedCharRange.length == 0 && 
+          [replacementString isEqualToString:@"\n"])
+      {
+        // Possibly unintended newline. Should display shortcut tip if no text is entered.
+        addedNewLine = YES;
+        [self performSelector:@selector(showShortcutTip) withObject:nil afterDelay:0.7];
+      }
+    }
+    else
+    {
+      if (!removedNewLine &&
+          affectedCharRange.location == ([[aTextView string] length] - 1) && 
+          affectedCharRange.length == 1 && 
+          [replacementString isEqualToString:@""])
+      {
+        // backspace entered. This usually happens when unintended new line is cancelled. Ignore it.
+        removedNewLine = YES;
+      }
+      else
+      {
+        [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(showShortcutTip) object:nil];
+        finishedPlayingWithTooltip = YES;
+      }
+    }    
+  }
+  return YES;
+}
+
+
+- (void) showShortcutTip
+{
+  [self.shortcutTipLabel setHidden:NO];
+  finishedPlayingWithTooltip = YES;
+}
 
 
 @end
