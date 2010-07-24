b06f33d9321d9a063d0b9afc989db0e469cdd5d9
Oleg Andreev <oleganza@gmail.com>
2010-07-25 01:11:00 +0200

    wip on license window

--- GBLicenseController.h
+++ GBLicenseController.h
@@ -1,6 +1,13 @@
 @interface GBLicenseController : NSWindowController
 {
-//  NSProgressIndicator* progressIndicator;
 }
+
+@property (nonatomic, retain) IBOutlet NSProgressIndicator* progressIndicator;
+@property (nonatomic, retain) IBOutlet NSTextField* licenseField;
+@property (nonatomic, retain) IBOutlet NSLabel* progressLabel;
+
+- (IBAction) buy;
+- (IBAction) cancel;
+- (IBAction) ok;
 
 @end
--- GBLicenseController.m
+++ GBLicenseController.m
@@ -2,4 +2,34 @@
 
 @implementation GBLicenseController
 
+@synthesize progressIndicator;
+@synthesize licenseField;
+@synthesize progressLabel;
+
+- (void)dealloc
+{
+  self.progressIndicator = nil;
+  self.licenseField = nil;
+  self.progressLabel = nil;
+  [super dealloc];
+}
+
+
+- (IBAction) buy
+{
+  NSString* releaseNotesURLString = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"GBPurchaseURL"];
+  [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:releaseNotesURLString]];
+}
+
+- (IBAction) cancel
+{
+  
+}
+
+- (IBAction) ok
+{
+  
+}
+
+
 @end
