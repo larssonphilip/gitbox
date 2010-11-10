cb1b9f7636d1139b05042ea88a25692ef68a8273
Oleg Andreev <oleganza@gmail.com>
2010-11-11 00:22:31 +0100

    sample receipt seemed to work fine

--- main.m
+++ main.m
@@ -1,14 +1,31 @@
-//
-//  main.m
-//  gitbox
-//
-//  Created by Oleg Andreev on 5/3/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
+
+#define OA_APPSTORE_SAMPLE_RECEIPT 1
+
+#import "OAAppStoreReceipt.h"
 
 int main(int argc, char *argv[])
 {
-    return NSApplicationMain(argc, (const char **) argv);
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  
+  #if GITBOX_APP_STORE
+    // The purpose of this check here is to provide the user with
+    // a proper iTunes update (say, when she moves to another computer).
+    // Obfuscated check happens elsewhere in the app after some period of time.
+    NSString* receiptPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Contents/_MASReceipt/receipt"];
+    // put the example receipt on the desktop (or change that path)
+    #if OA_APPSTORE_SAMPLE_RECEIPT
+      receiptPath = @"/Users/oleganza/Work/gitbox/app/appstore_receipt_tests/SampleReceipt";
+    #endif
+    if (!OAValidateAppStoreReceiptAtPath(receiptPath))
+    {
+      NSLog(@"Gitbox main: receipt not found or not valid at path %@", receiptPath);
+      exit(173);
+      return 173;
+    }
+  #endif
+  
+  int code = NSApplicationMain(argc, (const char **) argv);
+  [pool drain];
+  return code;
 }
