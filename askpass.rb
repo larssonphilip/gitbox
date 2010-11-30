2933b0b537e59cb92cb626ae427e3e25aa0ba6ef
Oleg Andreev <oleganza@gmail.com>
2010-12-01 00:59:05 +0100

    askpass fixes: focuses Gitbox, normal icon, keychain support

--- askpass.rb
+++ askpass.rb
@@ -3,7 +3,7 @@
 
 def main
   title = ENV["MACOS_ASKPASS_TITLE"]
-  title = "SSH" if title.to_s.strip == ""
+  title = "Gitbox SSH" if title.to_s.strip == ""
   message = ARGV.join(" ").to_s.strip
   
   if message =~ /\(?yes\/no\)?\??$/
@@ -21,17 +21,42 @@ def main
   end
 end
 
+def keychain_service_name_for_string(string)
+  string.to_s.gsub(/[^a-z0-9A-Z\,\.]+/," ").gsub(/\s+/," ").strip
+end
+
+def password_from_keychain(service)
+  pass = `security find-generic-password -gs #{double_quote(service)} 2>&1 >/dev/null | cut -d '"' -f 2`.strip
+  return '' if pass =~ /SecKeychainSearchCopyNext:/
+  pass
+end
+
+def store_password_in_keychain(password, keychain)
+  user_name = `$USER`.to_s.strip
+  `security add-generic-password -a #{double_quote(user_name)} -s #{double_quote(keychain)} -w #{double_quote(password)} -U`
+end
+
 def password_prompt(title, message)
-  dialog = %{display dialog #{double_quote(message)} default answer "" with title #{double_quote(title)}  with icon caution with hidden answer} 
+  dialog = %{display dialog #{double_quote(message)} default answer "" with title #{double_quote(title)}  with icon note with hidden answer} 
   
-  result = `osascript -e 'tell application "Finder"' -e "activate"  -e #{double_quote(dialog)} -e 'end tell'`.to_s
-  return result.gsub(/, button returned:.*/mi, "").gsub(/.*text returned:/,"").strip
+  keychain = keychain_service_name_for_string(title.to_s + ": " + message.to_s)
+  password = password_from_keychain(keychain)
+  if password.to_s != ""
+    return password
+  end
+  
+  result = `osascript -e 'tell application "Gitbox"' -e "activate"  -e #{double_quote(dialog)} -e 'end tell'`.to_s
+  password = result.gsub(/, button returned:.*/mi, "").gsub(/.*text returned:/,"").strip
+  
+  store_password_in_keychain(password, keychain)
+  
+  password
 end
 
 def yes_no_prompt(title, message)
-  dialog = %{display dialog #{double_quote(message)} buttons {"No", "Yes"} default button 2 with title #{double_quote(title)} with icon caution} 
+  dialog = %{display dialog #{double_quote(message)} buttons {"No", "Yes"} default button 2 with title #{double_quote(title)} with icon note} 
   
-  result = `osascript -e 'tell application "Finder"' -e "activate"  -e #{double_quote(dialog)} -e 'end tell'`.to_s
+  result = `osascript -e 'tell application "Gitbox"' -e "activate"  -e #{double_quote(dialog)} -e 'end tell'`.to_s
   
   if result =~ /returned:Yes/i
     return 'yes'
