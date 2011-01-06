c578a0f9e97b9f57b0c2378be3a9845571910310
Oleg Andreev <oleganza@gmail.com>
2011-01-06 14:05:08 +0100

    wip on GBRepositoriesGroup

---
+++ Classes/GBRepositoriesAndGroupsSerialization.h
@@ -0,0 +1,10 @@
+#import "GBRepository.h"
+#import "GBRepositoriesGroup.h"
+
+// TODO: define common methods to convert to/from preferences 
+
+@interface GBRepository (GBRepositoriesAndGroupsSerialization)
+@end
+
+@interface GBRepositoriesGroup (GBRepositoriesAndGroupsSerialization)
+@end
