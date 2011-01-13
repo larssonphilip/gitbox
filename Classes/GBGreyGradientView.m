f44b3746f3479745e2a3fd84aeaa4b800990a329
Oleg Andreev <oleganza@gmail.com>
2011-01-13 12:50:17 +0100

    correct + more efficient line drawing in GBGreyGradientView

--- Classes/GBGreyGradientView.m
+++ Classes/GBGreyGradientView.m
@@ -19,6 +19,8 @@
 
 - (void) drawRect:(NSRect)dirtyRect
 {
+  NSRect bounds = self.bounds;
+  
   if (!self.gradient)
   {
     self.gradient = [[NSGradient alloc]
@@ -26,21 +28,16 @@
                              endingColor:[NSColor colorWithCalibratedWhite:208.0/255.0 alpha:1.0]];
   }
   
-  [self.gradient drawInRect:self.bounds angle:270];
+  [self.gradient drawInRect:bounds angle:270];
   
 
   if (!self.lineColor)
   {
-    self.lineColor = [NSColor colorWithCalibratedWhite:126.0/255.0 alpha:1.0];
+    self.lineColor = [NSColor colorWithCalibratedWhite:166.0/255.0 alpha:1.0];
   }
   
   [self.lineColor set];
-  {
-    NSBezierPath* bezierPath = [NSBezierPath bezierPath];
-    [bezierPath moveToPoint:NSMakePoint(NSMinX(self.bounds), NSMinY(self.bounds))];
-    [bezierPath lineToPoint:NSMakePoint(NSMaxX(self.bounds), NSMinY(self.bounds))];
-    [bezierPath stroke];
-  }
+  [NSBezierPath fillRect:NSMakeRect(bounds.origin.x, bounds.origin.y, bounds.size.width, 1.0)];
 }
 
 @end
