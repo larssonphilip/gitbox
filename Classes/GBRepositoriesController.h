c45e5e0c991c6b5834e70f73ca359e27f54c468f
Oleg Andreev <oleganza@gmail.com>
2011-03-01 13:48:27 +0100

    cleanup in repos ctrl

--- Classes/GBRepositoriesController.h
+++ Classes/GBRepositoriesController.h
@@ -12,28 +12,4 @@
 @property(nonatomic, retain) GBRepositoryViewController* repositoryViewController;
 @property(nonatomic, retain) GBRepositoryToolbarController* repositoryToolbarController;
 
-
-//@property(assign) id<GBRepositoriesControllerDelegate> delegate;
-
-//- (GBBaseRepositoryController*) openedLocalRepositoryControllerWithURL:(NSURL*)url;
-
-//- (GBRepositoryController*) selectedLocalRepositoryController;
-//- (GBRepositoryCloningController*) selectedCloningRepositoryController;
-
-//- (void) openLocalRepositoryAtURL:(NSURL*)url;
-//- (void) openLocalRepositoryAtURL:(NSURL*)url inGroup:(GBRepositoriesGroup*)aGroup atIndex:(NSInteger)anIndex;
-//
-//- (void) addLocalRepositoryController:(GBBaseRepositoryController*)repoCtrl;
-//- (void) addLocalRepositoryController:(GBBaseRepositoryController*)repoCtrl inGroup:(GBRepositoriesGroup*)aGroup atIndex:(NSInteger)anIndex;
-
-//- (void) removeLocalRepositoriesGroup:(GBRepositoriesGroup*)aGroup;
-//- (void) removeLocalRepositoryController:(GBBaseRepositoryController*)repoCtrl;
-
-//- (void) addGroup:(GBRepositoriesGroup*)aGroup inGroup:(GBRepositoriesGroup*)aGroup atIndex:(NSInteger)anIndex;
-
-//- (void) loadLocalRepositoriesAndGroups;
-//- (void) saveLocalRepositoriesAndGroups;
-
-//- (void) doWithSelectedGroupAtIndex:(void(^)(GBRepositoriesGroup* aGroup, NSInteger anIndex))aBlock;
-
 @end
--- Classes/GBRepositoriesController.m
+++ Classes/GBRepositoriesController.m
@@ -16,12 +16,19 @@
 #import "NSObject+OASelectorNotifications.h"
 
 
+@interface GBRepositoriesController ()
+@property(nonatomic, assign) BOOL itemsRestoredFromPreferences;
+- (void) loadLocalRepositoriesAndGroups;
+- (void) saveLocalRepositoriesAndGroups;
+@end
+
 @implementation GBRepositoriesController
 
 @synthesize localRepositoriesUpdatesQueue;
 @synthesize autofetchQueue;
 @synthesize repositoryViewController;
 @synthesize repositoryToolbarController;
+@synthesize itemsRestoredFromPreferences;
 
 
 - (void) dealloc
@@ -60,6 +67,15 @@
 #pragma mark GBSidebarItem
 
 
+- (NSInteger) sidebarItemNumberOfChildren
+{
+  if (!self.itemsRestoredFromPreferences)
+  {
+    [self loadLocalRepositoriesAndGroups];
+    self.itemsRestoredFromPreferences = YES;
+  }
+  return [super sidebarItemNumberOfChildren];
+}
 
 
 
@@ -105,8 +121,6 @@
 
 
 
-/*
-
 - (NSURL*) URLFromBookmarkData:(NSData*)bookmarkData
 {
   if (!bookmarkData) return nil;
@@ -122,140 +136,141 @@
   return aURL;
 }
 
-- (GBRepositoryController*) localItemFromURLBookmark:(NSData*)bookmarkData
-{
-  NSURL* aURL = [self URLFromBookmarkData:bookmarkData];
-  if (!aURL) return nil;
-  if ([GBRepository isValidRepositoryPath:[aURL path]])
-  {
-    return [GBRepositoryController repositoryControllerWithURL:aURL];
-  }
-  return nil;
-}
 
-// Returns GBRepositoryController or GBRepositoriesGroup
-- (id<GBRepositoriesControllerLocalItem>) localItemFromPlist:(id)plist
-{
-  if (!plist) return nil;
-  if (![plist isKindOfClass:[NSDictionary class]]) return nil;
-  
-  NSData* bookmarkData = [plist objectForKey:@"URL"];
-  if (bookmarkData)
-  {
-    return [self localItemFromURLBookmark:bookmarkData];
-  }
-  
-  NSString* groupName = [plist objectForKey:@"name"];
-  NSArray* groupItems = [plist objectForKey:@"items"];
-  NSNumber* groupIsExpanded = [plist objectForKey:@"isExpanded"];
-  
-  if (!groupName) return nil;
-  if (![groupName isKindOfClass:[NSString class]]) return nil;
+//- (GBRepositoryController*) localItemFromURLBookmark:(NSData*)bookmarkData
+//{
+//  NSURL* aURL = [self URLFromBookmarkData:bookmarkData];
+//  if (!aURL) return nil;
+//  if ([GBRepository isValidRepositoryPath:[aURL path]])
+//  {
+//    return [GBRepositoryController repositoryControllerWithURL:aURL];
+//  }
+//  return nil;
+//}
 
-  if (!groupItems) return nil;
-  if (![groupItems isKindOfClass:[NSArray class]]) return nil;
-  
-  GBRepositoriesGroup* aGroup = [[[GBRepositoriesGroup alloc] init] autorelease];
-  
-  aGroup.name = groupName;
-  [aGroup setExpandedInSidebar:(groupIsExpanded ? [groupIsExpanded boolValue] : NO)];
-  
-  for (id subitemPlist in groupItems)
-  {
-    id<GBRepositoriesControllerLocalItem> subitem = [self localItemFromPlist:subitemPlist];
-    if (subitem)
-    {
-      [aGroup.items addObject:subitem];
-    }
-  }
-  
-  return aGroup;
-}
+// // Returns GBRepositoryController or GBRepositoriesGroup
+//- (id<GBRepositoriesControllerLocalItem>) localItemFromPlist:(id)plist
+//{
+//  if (!plist) return nil;
+//  if (![plist isKindOfClass:[NSDictionary class]]) return nil;
+//  
+//  NSData* bookmarkData = [plist objectForKey:@"URL"];
+//  if (bookmarkData)
+//  {
+//    return [self localItemFromURLBookmark:bookmarkData];
+//  }
+//  
+//  NSString* groupName = [plist objectForKey:@"name"];
+//  NSArray* groupItems = [plist objectForKey:@"items"];
+//  NSNumber* groupIsExpanded = [plist objectForKey:@"isExpanded"];
+//  
+//  if (!groupName) return nil;
+//  if (![groupName isKindOfClass:[NSString class]]) return nil;
+//
+//  if (!groupItems) return nil;
+//  if (![groupItems isKindOfClass:[NSArray class]]) return nil;
+//  
+//  GBRepositoriesGroup* aGroup = [[[GBRepositoriesGroup alloc] init] autorelease];
+//  
+//  aGroup.name = groupName;
+//  [aGroup setExpandedInSidebar:(groupIsExpanded ? [groupIsExpanded boolValue] : NO)];
+//  
+//  for (id subitemPlist in groupItems)
+//  {
+//    id<GBRepositoriesControllerLocalItem> subitem = [self localItemFromPlist:subitemPlist];
+//    if (subitem)
+//    {
+//      [aGroup.items addObject:subitem];
+//    }
+//  }
+//  
+//  return aGroup;
+//}
 
  - (void) loadLocalRepositoriesAndGroups
 {
-  GBBaseRepositoryController* selectedRepoCtrl = nil;
-  
-  NSDictionary* localRepositoriesGroupPlist = [[NSUserDefaults standardUserDefaults] objectForKey:@"GBRepositoriesController_localRepositoriesGroup"];
-  
-  if (localRepositoriesGroupPlist)
-  {
-    if (![localRepositoriesGroupPlist isKindOfClass:[NSDictionary class]]) return;
-    id<GBRepositoriesControllerLocalItem> localItem = [self localItemFromPlist:localRepositoriesGroupPlist];
-    if (localItem && [localItem isKindOfClass:[GBRepositoriesGroup class]])
-    {
-      self.localRepositoriesGroup.items = ((GBRepositoriesGroup*)localItem).items;
-    }
-  }
-  else
-  {
-    // Load repos from the legacy format (<= v1.1)
-    
-    NSArray* bookmarks1_1 = [[NSUserDefaults standardUserDefaults] objectForKey:@"GBRepositoriesController_localRepositories"];
-    if (![bookmarks1_1 isKindOfClass:[NSArray class]]) return;
-    
-    for (NSData* bookmarkData in bookmarks1_1)
-    {
-      GBBaseRepositoryController* repoCtrl = [self localItemFromURLBookmark:bookmarkData];
-      if (repoCtrl) [self.localRepositoriesGroup.items addObject:repoCtrl];
-    }
-  }
-  
-  __block GBBaseRepositoryController* firstRepoCtrl = nil;
-  [self.localRepositoriesGroup enumerateRepositoriesWithBlock:^(GBBaseRepositoryController* repoCtrl){
-    if (!firstRepoCtrl) firstRepoCtrl = repoCtrl;
-    [self launchRepositoryController:repoCtrl queued:YES];
-  }];
-  
-  NSData* selectedLocalRepoBoomarkData = [[NSUserDefaults standardUserDefaults] objectForKey:@"GBRepositoriesController_selectedLocalRepository"];
-  
-  NSURL* selectedURL = [self URLFromBookmarkData:selectedLocalRepoBoomarkData];
-  
-//  if ([self.delegate respondsToSelector:@selector(repositoriesControllerDidLoadLocalRepositoriesAndGroups:)]) { [self.delegate repositoriesControllerDidLoadLocalRepositoriesAndGroups:self]; }
-
-  if (selectedURL)
-  {
-    selectedRepoCtrl = [self openedLocalRepositoryControllerWithURL:selectedURL];
-  }
-  
-  if (!selectedRepoCtrl)
-  {
-    selectedRepoCtrl = firstRepoCtrl;
-  }
-  
-  //[self selectRepositoryController:selectedRepoCtrl];
+//  GBBaseRepositoryController* selectedRepoCtrl = nil;
+//  
+//  NSDictionary* localRepositoriesGroupPlist = [[NSUserDefaults standardUserDefaults] objectForKey:@"GBRepositoriesController_localRepositoriesGroup"];
+//  
+//  if (localRepositoriesGroupPlist)
+//  {
+//    if (![localRepositoriesGroupPlist isKindOfClass:[NSDictionary class]]) return;
+//    id<GBRepositoriesControllerLocalItem> localItem = [self localItemFromPlist:localRepositoriesGroupPlist];
+//    if (localItem && [localItem isKindOfClass:[GBRepositoriesGroup class]])
+//    {
+//      self.localRepositoriesGroup.items = ((GBRepositoriesGroup*)localItem).items;
+//    }
+//  }
+//  else
+//  {
+//    // Load repos from the legacy format (<= v1.1)
+//    
+//    NSArray* bookmarks1_1 = [[NSUserDefaults standardUserDefaults] objectForKey:@"GBRepositoriesController_localRepositories"];
+//    if (![bookmarks1_1 isKindOfClass:[NSArray class]]) return;
+//    
+//    for (NSData* bookmarkData in bookmarks1_1)
+//    {
+//      GBBaseRepositoryController* repoCtrl = [self localItemFromURLBookmark:bookmarkData];
+//      if (repoCtrl) [self.localRepositoriesGroup.items addObject:repoCtrl];
+//    }
+//  }
+//  
+//  __block GBBaseRepositoryController* firstRepoCtrl = nil;
+//  [self.localRepositoriesGroup enumerateRepositoriesWithBlock:^(GBBaseRepositoryController* repoCtrl){
+//    if (!firstRepoCtrl) firstRepoCtrl = repoCtrl;
+//    [self launchRepositoryController:repoCtrl queued:YES];
+//  }];
+//  
+//  NSData* selectedLocalRepoBoomarkData = [[NSUserDefaults standardUserDefaults] objectForKey:@"GBRepositoriesController_selectedLocalRepository"];
+//  
+//  NSURL* selectedURL = [self URLFromBookmarkData:selectedLocalRepoBoomarkData];
+//  
+////  if ([self.delegate respondsToSelector:@selector(repositoriesControllerDidLoadLocalRepositoriesAndGroups:)]) { [self.delegate repositoriesControllerDidLoadLocalRepositoriesAndGroups:self]; }
+//
+//  if (selectedURL)
+//  {
+//    selectedRepoCtrl = [self openedLocalRepositoryControllerWithURL:selectedURL];
+//  }
+//  
+//  if (!selectedRepoCtrl)
+//  {
+//    selectedRepoCtrl = firstRepoCtrl;
+//  }
+//  
+//  //[self selectRepositoryController:selectedRepoCtrl];
 }
 
 
 
 - (void) saveLocalRepositoriesAndGroups
 {
-  id localRepositoriesGroupPlist = [self.localRepositoriesGroup plistRepresentationForUserDefaults];
-    
-  [[NSUserDefaults standardUserDefaults] setObject:localRepositoriesGroupPlist 
-                                            forKey:@"GBRepositoriesController_localRepositoriesGroup"];
-  
-  NSData* selectedLocalRepoBoomarkData = nil;
-//  if (self.selectedRepositoryController)
+//  id localRepositoriesGroupPlist = [self.localRepositoriesGroup plistRepresentationForUserDefaults];
+//    
+//  [[NSUserDefaults standardUserDefaults] setObject:localRepositoriesGroupPlist 
+//                                            forKey:@"GBRepositoriesController_localRepositoriesGroup"];
+//  
+//  NSData* selectedLocalRepoBoomarkData = nil;
+////  if (self.selectedRepositoryController)
+////  {
+////    selectedLocalRepoBoomarkData = [[self.selectedRepositoryController url] bookmarkDataWithOptions:NSURLBookmarkCreationMinimalBookmark
+////                                    includingResourceValuesForKeys:nil
+////                                                     relativeToURL:nil
+////                                                             error:NULL]; 
+////  }
+//  
+//  if (selectedLocalRepoBoomarkData)
 //  {
-//    selectedLocalRepoBoomarkData = [[self.selectedRepositoryController url] bookmarkDataWithOptions:NSURLBookmarkCreationMinimalBookmark
-//                                    includingResourceValuesForKeys:nil
-//                                                     relativeToURL:nil
-//                                                             error:NULL]; 
+//    [[NSUserDefaults standardUserDefaults] setObject:selectedLocalRepoBoomarkData 
+//                                              forKey:@"GBRepositoriesController_selectedLocalRepository"];
 //  }
-  
-  if (selectedLocalRepoBoomarkData)
-  {
-    [[NSUserDefaults standardUserDefaults] setObject:selectedLocalRepoBoomarkData 
-                                              forKey:@"GBRepositoriesController_selectedLocalRepository"];
-  }
-  else
-  {
-    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"GBRepositoriesController_selectedLocalRepository"];
-  }
+//  else
+//  {
+//    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"GBRepositoriesController_selectedLocalRepository"];
+//  }
 }
 
-*/
+
 
 
 @end
