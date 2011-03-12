132ed1439ff2350975c578cf5aa1ee5c60fb084b
Oleg Andreev <oleganza@gmail.com>
2011-03-12 16:44:47 +0100

    removed GBNotificationMacros

--- Classes/GBNotificationMacros.h
+++
@@ -1,4 +0,0 @@
-
-#define GBNotificationDeclare(NAME) extern NSString* const NAME;
-#define GBNotificationDefine(NAME) NSString* const NAME = @#NAME;
-#define GBNotificationSend(NAME) [[NSNotificationCenter defaultCenter] postNotificationName:NAME object:self]
