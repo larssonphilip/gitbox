e635555d0052730c4bb2e999d1f62c8613ee02aa
Oleg Andreev <oleganza@gmail.com>
2011-09-26 07:31:39 +0200

    indentation

--- Classes/GBHistoryTableView.m
+++ Classes/GBHistoryTableView.m
@@ -7,42 +7,42 @@
 - (NSMenu *) menuForEvent:(NSEvent *) event
 {
 	NSInteger rowIndex = -1;
-  NSInteger colIndex = -1;
+	NSInteger colIndex = -1;
 	
 	NSPoint point = [self convertPoint:[event locationInWindow] fromView:nil];
 	rowIndex = [self rowAtPoint:point];
 	colIndex = [self columnAtPoint:point];
 	
 	if (rowIndex < 0)
-  {
-    [self deselectAll:nil];
-    return [super menuForEvent:event];
-  }
+	{
+		[self deselectAll:nil];
+		return [super menuForEvent:event];
+	}
     
-  NSTableColumn* column = nil;
-  if (colIndex >= 0)
-  {
-    column = [[self tableColumns] objectAtIndex:colIndex];
-  }
-  
-  if ([[self delegate] respondsToSelector:@selector(tableView:shouldSelectRow:)])
-  {
-    if ([[self delegate] tableView:self shouldSelectRow:rowIndex])
-    {
-      [self selectRowIndexes:[NSIndexSet indexSetWithIndex:(NSUInteger)rowIndex] byExtendingSelection:NO];
-    }
-  }
-  else
-  {
-    [self selectRowIndexes:[NSIndexSet indexSetWithIndex:(NSUInteger)rowIndex] byExtendingSelection:NO];
-  }
-  
-  if ([[self dataSource] respondsToSelector:@selector(tableView:menuForTableColumn:row:)])
-  {
-    return [(id<GBHistoryTableViewDataSource>)[self dataSource] tableView:self menuForTableColumn:column row:rowIndex];
-  }
-  
-  return [super menuForEvent:event];
+	NSTableColumn* column = nil;
+	if (colIndex >= 0)
+	{
+		column = [[self tableColumns] objectAtIndex:colIndex];
+	}
+	
+	if ([[self delegate] respondsToSelector:@selector(tableView:shouldSelectRow:)])
+	{
+		if ([[self delegate] tableView:self shouldSelectRow:rowIndex])
+		{
+			[self selectRowIndexes:[NSIndexSet indexSetWithIndex:(NSUInteger)rowIndex] byExtendingSelection:NO];
+		}
+	}
+	else
+	{
+		[self selectRowIndexes:[NSIndexSet indexSetWithIndex:(NSUInteger)rowIndex] byExtendingSelection:NO];
+	}
+	
+	if ([[self dataSource] respondsToSelector:@selector(tableView:menuForTableColumn:row:)])
+	{
+		return [(id<GBHistoryTableViewDataSource>)[self dataSource] tableView:self menuForTableColumn:column row:rowIndex];
+	}
+	
+	return [super menuForEvent:event];
 }
 
 @end
