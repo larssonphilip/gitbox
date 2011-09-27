cc440d599e864eb52d606f852cc263a2ca2beb0e
Oleg Andreev <oleganza@gmail.com>
2011-09-27 09:10:14 +0200

    wip

--- Classes/GBRepositorySummaryController.m
+++ Classes/GBRepositorySummaryController.m
@@ -6,15 +6,21 @@
 #import "GitConfig.h"
 
 @interface GBRepositorySummaryController ()
+@property(nonatomic, retain) NSArray* remotes;
+@property(nonatomic, retain) NSArray* labels;
+@property(nonatomic, retain) NSArray* fields;
+
 - (NSString*) parentFolder;
 - (NSString*) repoTitle;
 - (NSString*) repoPath;
-- (NSString*) repoURLString;
-- (NSString*) repoURLString:(NSUInteger)anIndex;
 - (void) hideRemoteAddressFieldsCount:(int)numberOfFieldsToHide;
 @end
 
 @implementation GBRepositorySummaryController
+
+@synthesize remotes;
+@synthesize labels;
+@synthesize fields;
 
 @synthesize pathLabel;
 @synthesize originLabel;
@@ -31,6 +37,10 @@
 
 - (void) dealloc
 {
+	self.remotes = nil;
+	self.labels = nil;
+	self.fields = nil;
+	
 	self.pathLabel = nil;
 	self.originLabel = nil;  
 	[super dealloc];
@@ -55,26 +65,62 @@
 	
 	[self.pathLabel setStringValue:self.repoPath];
 	
-	NSArray* remotes = self.repository.remotes;
-	if (remotes.count == 0)
+	self.labels = [NSArray arrayWithObjects:self.remoteLabel1, self.remoteLabel2, self.remoteLabel3, nil];
+	self.fields = [NSArray arrayWithObjects:self.remoteField1, self.remoteField2, self.remoteField3, nil];
+
+	// initialize tags so they are invalid
+	for (NSTextField* field in fields)
+	{
+		field.tag = -1;
+	}
+	
+	self.remotes = self.repository.remotes;
+	
+	NSUInteger linesCount = MIN(remotes.count, self.fields.count);
+	
+	if (linesCount == 0)
 	{
 		self.remoteLabel1.stringValue = NSLocalizedString(@"Remote address:", @"");
 		self.remoteField1.stringValue = @"";
-		[self hideRemoteAddressFieldsCount:2];
+		self.remoteField1.tag = 0;
+		linesCount = 1;
 	}
-	else if (remotes.count == 1)
+	else if (linesCount == 1)
 	{
 		self.remoteLabel1.stringValue = NSLocalizedString(@"Remote address:", @"");
-		self.remoteField1.stringValue = [[remotes objectAtIndex:0] URLString];
-
-		[self hideRemoteAddressFieldsCount:2];
+		NSString* str = [[remotes objectAtIndex:0] URLString];
+		self.remoteField1.stringValue = str ? str : @"";
+		self.remoteField1.tag = 0;
 	}
 	else
 	{
-		
+		for (NSUInteger i = 0; i < linesCount; i++)
+		{
+			NSTextField* field = [self.fields objectAtIndex:i];
+			NSTextField* label = [self.labels objectAtIndex:i];
+			GBRemote* remote   = [self.remotes objectAtIndex:i];
+			
+			label.stringValue = [NSString stringWithFormat:NSLocalizedString(@"Remote address (%@):", @""), remote.alias];
+			NSString* str = [remote URLString];
+			field.stringValue = str ? str : @"";
+			field.tag = i;
+		}
 	}
 	
-	[self.originLabel setStringValue:self.repoURLString];
+	CGFloat remainingViewOffset = 0.0;
+	
+	for (NSUInteger i = linesCount; i < fields.count; i++)
+	{
+		NSTextField* f = [self.labels objectAtIndex:i];
+		[f setHidden:YES];
+		f = [self.fields objectAtIndex:i];
+		[f setHidden:YES];
+		remainingViewOffset += 38 + 17;
+	}
+	
+	NSRect rect = self.remainingView.frame;
+	rect.size.height += remainingViewOffset;
+	
 	
 	[self.repository.libgitRepository.config enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
 		NSLog(@"Config: %@ => %@", key, obj);
@@ -96,6 +142,9 @@
 - (void) save
 {
 	// TODO: save the remote addresses
+	// self.remotes
+	
+	
 }
 
 
@@ -158,16 +207,15 @@
 
 - (IBAction) optimizeRepository:(NSButton*)button
 {
-	// TODO: launch "git gc", monitor status
-	
 	NSString* originalTitle = button.title;
 	[button setEnabled:NO];
-	button.title = NSLocalizedString(@"Optimizing...", @"");
+	NSString* aTitle = NSLocalizedString(@"Optimizing...", @"");
+	button.title = aTitle;
 	
 	GBTaskWithProgress* gitgcTask = [GBTaskWithProgress taskWithRepository:self.repository];
 	gitgcTask.arguments = [NSArray arrayWithObject:@"gc"];
 	gitgcTask.progressUpdateBlock = ^{
-		button.title = [NSString stringWithFormat:@"%@ %d%", NSLocalizedString(@"Optimizing...", @""), (int)roundf(gitgcTask.progress*100.0)];
+		button.title = [NSString stringWithFormat:@"%@ %d%", aTitle, (int)roundf(gitgcTask.progress*100.0)];
 	};
 	[gitgcTask launchWithBlock:^{
 		[button setEnabled:YES];
--- Classes/GBTask.m
+++ Classes/GBTask.m
@@ -13,16 +13,16 @@
 
 + (id) taskWithRepository:(GBRepository*)repo
 {
-  GBTask* task = [self task];
-  task.repository = repo;
-  return task;
+	GBTask* task = [self task];
+	task.repository = repo;
+	return task;
 }
 
 - (id) copyWithZone:(NSZone *)zone
 {
-  GBTask* newTask = [super copyWithZone:zone];
-  newTask.repository = self.repository;
-  return newTask;
+	GBTask* newTask = [super copyWithZone:zone];
+	newTask.repository = self.repository;
+	return newTask;
 }
 
 
@@ -30,77 +30,77 @@
 
 + (NSString*) bundledGitVersion
 {
-  return @"1.7.5.4";
+	return @"1.7.5.4";
 }
 
 // TODO: future improvement here: do not remove bundled tar and unpack to Application Support folder instead of bundle to enable packed binary for the App Store.
 + (NSString*) pathToBundledBinary:(NSString*)name
 {
-  static NSString* cachedPathToGitBinary = nil;
-  if (cachedPathToGitBinary && [name isEqual:@"git"]) return cachedPathToGitBinary;
-  
-  NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
-  
-  if ([paths count] < 1)
-  {
-    NSLog(@"GBTask: Application Support directory is not found using NSSearchPathForDirectoriesInDomains!");
-    return nil;
-  }
-  
-  NSString* applicationSupportPath = [paths objectAtIndex:0];
-  NSString* gitboxAppSupportPath = [applicationSupportPath stringByAppendingPathComponent:@"Gitbox"];
-  NSString* pathToBundle = [gitboxAppSupportPath stringByAppendingPathComponent:[NSString stringWithFormat:@"git-%@/git.bundle", [self bundledGitVersion]]];
-  
-  NSFileManager* fm = [[[NSFileManager alloc] init] autorelease];
-  
-  if (![fm fileExistsAtPath:pathToBundle])
-  {
-    NSError* error = nil;
-    NSString* parentBundlePath = [pathToBundle stringByDeletingLastPathComponent];
-    if (![fm createDirectoryAtPath:parentBundlePath
-      withIntermediateDirectories:YES 
-                       attributes:nil 
-                            error:&error])
-    {
-      NSLog(@"ERROR: GBTask: cannot create directory %@", parentBundlePath);
-      NSAssert(0, @"GBTask: cannot create directory for extracted git.bundle!");
-      return nil;
-    }
-
-    NSString* pathToTar = [[NSBundle mainBundle] pathForResource:@"git.bundle" ofType:@"tar"];
-    if (!pathToTar)
-    {
-      NSLog(@"ERROR: Missing git.bundle.tar in the application package!");
-      NSAssert(0, @"Missing git.bundle.tar in the application package!");
-      return nil;
-    }
-    
-    OATask* unpackTask = [OATask task];
-    unpackTask.executableName = @"tar";
-    unpackTask.arguments = [NSArray arrayWithObjects:@"-xf", pathToTar, @"-C", parentBundlePath, nil];
-    unpackTask.currentDirectoryPath = [pathToTar stringByDeletingLastPathComponent];
-    [unpackTask launchAndWait];
-    
-    // Note: asking nsbundle for the path again won't work: needs some time. (So we don't.)
-  }
-  
-  if (!pathToBundle)
-  {
-    NSLog(@"ERROR: pathToBundle is nil!");
-    NSAssert(0, @"pathToBundle is nil");
-    return nil;
-  }
-  
-  NSBundle* gitBundle = [NSBundle bundleWithPath:pathToBundle];
-  NSString* pathToBinary = [gitBundle pathForResource:name ofType:nil inDirectory:@"libexec/git-core"];
-  
-  if ([name isEqual:@"git"]) cachedPathToGitBinary = [pathToBinary retain];
-  return pathToBinary;
+	static NSString* cachedPathToGitBinary = nil;
+	if (cachedPathToGitBinary && [name isEqual:@"git"]) return cachedPathToGitBinary;
+	
+	NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+	
+	if ([paths count] < 1)
+	{
+		NSLog(@"GBTask: Application Support directory is not found using NSSearchPathForDirectoriesInDomains!");
+		return nil;
+	}
+	
+	NSString* applicationSupportPath = [paths objectAtIndex:0];
+	NSString* gitboxAppSupportPath = [applicationSupportPath stringByAppendingPathComponent:@"Gitbox"];
+	NSString* pathToBundle = [gitboxAppSupportPath stringByAppendingPathComponent:[NSString stringWithFormat:@"git-%@/git.bundle", [self bundledGitVersion]]];
+	
+	NSFileManager* fm = [[[NSFileManager alloc] init] autorelease];
+	
+	if (![fm fileExistsAtPath:pathToBundle])
+	{
+		NSError* error = nil;
+		NSString* parentBundlePath = [pathToBundle stringByDeletingLastPathComponent];
+		if (![fm createDirectoryAtPath:parentBundlePath
+		   withIntermediateDirectories:YES 
+							attributes:nil 
+								 error:&error])
+		{
+			NSLog(@"ERROR: GBTask: cannot create directory %@", parentBundlePath);
+			NSAssert(0, @"GBTask: cannot create directory for extracted git.bundle!");
+			return nil;
+		}
+		
+		NSString* pathToTar = [[NSBundle mainBundle] pathForResource:@"git.bundle" ofType:@"tar"];
+		if (!pathToTar)
+		{
+			NSLog(@"ERROR: Missing git.bundle.tar in the application package!");
+			NSAssert(0, @"Missing git.bundle.tar in the application package!");
+			return nil;
+		}
+		
+		OATask* unpackTask = [OATask task];
+		unpackTask.executableName = @"tar";
+		unpackTask.arguments = [NSArray arrayWithObjects:@"-xf", pathToTar, @"-C", parentBundlePath, nil];
+		unpackTask.currentDirectoryPath = [pathToTar stringByDeletingLastPathComponent];
+		[unpackTask launchAndWait];
+		
+		// Note: asking nsbundle for the path again won't work: needs some time. (So we don't.)
+	}
+	
+	if (!pathToBundle)
+	{
+		NSLog(@"ERROR: pathToBundle is nil!");
+		NSAssert(0, @"pathToBundle is nil");
+		return nil;
+	}
+	
+	NSBundle* gitBundle = [NSBundle bundleWithPath:pathToBundle];
+	NSString* pathToBinary = [gitBundle pathForResource:name ofType:nil inDirectory:@"libexec/git-core"];
+	
+	if ([name isEqual:@"git"]) cachedPathToGitBinary = [pathToBinary retain];
+	return pathToBinary;
 }
 
 - (NSString*) executableName
 {
-  return @"git";
+	return @"git";
 }
 
 
@@ -110,12 +110,12 @@
 
 - (NSString*) launchPath
 {
-  return [GBTask pathToBundledBinary:self.executableName];
+	return [GBTask pathToBundledBinary:self.executableName];
 }
 
 - (NSString*) currentDirectoryPath
 {
-  return [self.repository.url path];
+	return [self.repository.url path];
 }
 
 
@@ -124,12 +124,12 @@
 
 - (void) willLaunchTask
 {
-  if (!self.repository)
-  {
-    NSException *exception = [NSException exceptionWithName:@"RepositoryIsNil"
-                                                     reason:@"You may use [GBRepository task] to get a new configured GBTask"  userInfo:nil];
-    @throw exception;
-  }
+	if (!self.repository)
+	{
+		NSException *exception = [NSException exceptionWithName:@"RepositoryIsNil"
+														 reason:@"You may use [GBRepository task] to get a new configured GBTask"  userInfo:nil];
+		@throw exception;
+	}
 }
 
 @end
