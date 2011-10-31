2595fcde5431974c3ed3de1e581d674b7c4cf2df
Oleg Andreev <oleganza@gmail.com>
2011-10-31 18:14:09 +0100

    added entitlements; changed ask pass server name to static one

--- Classes/GBAskPassServer.m
+++ Classes/GBAskPassServer.m
@@ -20,17 +20,17 @@ NSString* const GBAskPassClientIdKey = @"GBAskPassClientId";
 
 - (void)dealloc
 {
-  self.name = nil;
-  self.clients = nil;
-  self.resultsByClientId = nil;
-  [self.connection invalidate];
-  self.connection = nil;
-  [super dealloc];
+	self.name = nil;
+	self.clients = nil;
+	self.resultsByClientId = nil;
+	[self.connection invalidate];
+	self.connection = nil;
+	[super dealloc];
 }
 
 + (GBAskPassServer*) sharedServer
 {
-  static id volatile instance = nil;
+	static id volatile instance = nil;
 	static dispatch_once_t once = 0;
 	dispatch_once( &once, ^{ instance = [[self alloc] init]; });
 	return instance;
@@ -38,109 +38,110 @@ NSString* const GBAskPassClientIdKey = @"GBAskPassClientId";
 
 + (NSDistantObject<GBAskPassServer>*) remoteServerWithName:(NSString*)aName
 {
-  NSDistantObject* distantObject = [NSConnection rootProxyForConnectionWithRegisteredName:aName host:nil];
-  [[distantObject retain] autorelease];
-  [distantObject setProtocolForProxy:@protocol(GBAskPassServer)];
-  return (NSDistantObject<GBAskPassServer>*)distantObject;
+	NSDistantObject* distantObject = [NSConnection rootProxyForConnectionWithRegisteredName:aName host:nil];
+	[[distantObject retain] autorelease];
+	[distantObject setProtocolForProxy:@protocol(GBAskPassServer)];
+	return (NSDistantObject<GBAskPassServer>*)distantObject;
 }
 
 - (id)init
 {
-  if ((self = [super init]))
-  {
-    self.name = [NSString stringWithFormat:@"GBAskPassServer-%d-%p", [[NSProcessInfo processInfo] processIdentifier], self];
-    self.clients = [NSMutableSet set];
-    self.resultsByClientId = [NSMutableDictionary dictionary];
-    // note: creating a retain cycle, call -invalidate to break it.
-    self.connection = [NSConnection serviceConnectionWithName:self.name rootObject:self];
-    if (!self.connection)
-    {
-      NSLog(@"GBAskPassServer: cannot create NSConnection with name %@", self.name);
-      [self release];
-      return nil;
-    }
-    [self.connection setDelegate:self];
-  }
-  return self;
+	if ((self = [super init]))
+	{
+		//    self.name = [NSString stringWithFormat:@"GBAskPassServer-%d-%p", [[NSProcessInfo processInfo] processIdentifier], self];
+		self.name = @"com.oleganza.gitbox.askpass";
+		self.clients = [NSMutableSet set];
+		self.resultsByClientId = [NSMutableDictionary dictionary];
+		// note: creating a retain cycle, call -invalidate to break it.
+		self.connection = [NSConnection serviceConnectionWithName:self.name rootObject:self];
+		if (!self.connection)
+		{
+			NSLog(@"GBAskPassServer: cannot create NSConnection with name %@", self.name);
+			[self release];
+			return nil;
+		}
+		[self.connection setDelegate:self];
+	}
+	return self;
 }
 
 - (void) invalidate
 {
-  [self.connection setRootObject:nil];
-  [self.connection invalidate];
+	[self.connection setRootObject:nil];
+	[self.connection invalidate];
 }
 
 - (void) addClient:(id<GBAskPassServerClient>)aClient
 {
-  [self.clients addObject:aClient];
+	[self.clients addObject:aClient];
 }
 
 - (void) removeClient:(id<GBAskPassServerClient>)aClient
 {
-  [self.clients removeObject:aClient];
+	[self.clients removeObject:aClient];
 }
 
 - (void) setResult:(NSString*)aResult forClientId:(NSString*) aClientId
 {
-  if (!aClientId) return;
-  if (aResult)
-  {
-    [self.resultsByClientId setObject:aResult forKey:aClientId];
-  }
-  else
-  {
-    [self.resultsByClientId removeObjectForKey:aClientId];
-  }
+	if (!aClientId) return;
+	if (aResult)
+	{
+		[self.resultsByClientId setObject:aResult forKey:aClientId];
+	}
+	else
+	{
+		[self.resultsByClientId removeObjectForKey:aClientId];
+	}
 }
 
 - (NSString*) resultForClient:(NSString*)clientId prompt:(NSString*)prompt environment:(NSDictionary*)environment
 {
-  if (!clientId) return nil;
-  NSString* preparedResult = [self.resultsByClientId objectForKey:clientId];
-  
-  // A client already set a result: return it and forget it.
-  if (preparedResult)
-  {
-    //NSLog(@"DEBUG: returning result %@", preparedResult);
-    [[preparedResult retain] autorelease];
-    [self.resultsByClientId removeObjectForKey:clientId];
-    return preparedResult;
-  }
-  
-  // Client has not yet set a result. Find a client object and ask it.
-  //NSLog(@"GBAskPassServer: resultForClient:%@ prompt:%@", clientId, prompt);
-  for (id<GBAskPassServerClient> aClient in self.clients)
-  {
-    if ([[aClient askPassClientId] isEqualToString:clientId])
-    {
-      return [aClient resultForClient:clientId prompt:prompt environment:environment];
-    }
-  }
-  
-//  #warning Temp debug code  
-//  double delayInSeconds = 2.0;
-//  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-//  dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//    NSString* r = [NSString stringWithFormat:@"Reply to %@", prompt];
-//    NSLog(@"DEBUG: setting result %@", r);
-//    [self setResult:r forClientId:clientId];
-//  });
-  
-  return nil;
+	if (!clientId) return nil;
+	NSString* preparedResult = [self.resultsByClientId objectForKey:clientId];
+	
+	// A client already set a result: return it and forget it.
+	if (preparedResult)
+	{
+		//NSLog(@"DEBUG: returning result %@", preparedResult);
+		[[preparedResult retain] autorelease];
+		[self.resultsByClientId removeObjectForKey:clientId];
+		return preparedResult;
+	}
+	
+	// Client has not yet set a result. Find a client object and ask it.
+	//NSLog(@"GBAskPassServer: resultForClient:%@ prompt:%@", clientId, prompt);
+	for (id<GBAskPassServerClient> aClient in self.clients)
+	{
+		if ([[aClient askPassClientId] isEqualToString:clientId])
+		{
+			return [aClient resultForClient:clientId prompt:prompt environment:environment];
+		}
+	}
+	
+	//  #warning Temp debug code  
+	//  double delayInSeconds = 2.0;
+	//  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+	//  dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+	//    NSString* r = [NSString stringWithFormat:@"Reply to %@", prompt];
+	//    NSLog(@"DEBUG: setting result %@", r);
+	//    [self setResult:r forClientId:clientId];
+	//  });
+	
+	return nil;
 }
 
 - (NSString*) echo:(NSString*)string
 {
-  return string;
+	return string;
 }
 
 - (NSString*) description
 {
-  return [NSString stringWithFormat:@"<GBAskPassServer:%p name:%@ clients:%d resultsByClientId:%@>", 
-          self, 
-          self.name, 
-          [self.clients count], 
-          self.resultsByClientId];
+	return [NSString stringWithFormat:@"<GBAskPassServer:%p name:%@ clients:%d resultsByClientId:%@>", 
+			self, 
+			self.name, 
+			[self.clients count], 
+			self.resultsByClientId];
 }
 
 @end
