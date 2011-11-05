a42e9221eab38d1b22702b21faa417b6269023dd
Oleg Andreev <oleganza@gmail.com>
2011-11-05 16:56:34 +0100

    todo upd

--- Classes/GBSearch.m
+++ Classes/GBSearch.m
@@ -33,42 +33,42 @@
 
 - (void) dealloc
 {
-  self.query = nil;
-  self.repository = nil;
-  self.commits = nil;
-  self.commitIds = nil;
-  
-  [searchCache release]; searchCache = nil;
-  
-  [self.task terminate];
-  self.task = nil;
-  [super dealloc];
+	self.query = nil;
+	self.repository = nil;
+	self.commits = nil;
+	self.commitIds = nil;
+	
+	[searchCache release]; searchCache = nil;
+	
+	[self.task terminate];
+	self.task = nil;
+	[super dealloc];
 }
 
 + (GBSearch*) searchWithQuery:(GBSearchQuery*)query repository:(GBRepository*)repo target:(id)target action:(SEL)action
 {
-  GBSearch* search = [[[self alloc] init] autorelease];
-  search.query = query;
-  search.repository = repo;
-  search.target = target;
-  search.action = action;
-  return search;
+	GBSearch* search = [[[self alloc] init] autorelease];
+	search.query = query;
+	search.repository = repo;
+	search.target = target;
+	search.action = action;
+	return search;
 }
 
 - (void) start
 {
-  self.commits = [NSMutableArray array];
-  self.commitIds = [NSMutableSet set];
-  self.limit = 50;
-  
-  [self launchNextTask];
+	self.commits = [NSMutableArray array];
+	self.commitIds = [NSMutableSet set];
+	self.limit = 50;
+	
+	[self launchNextTask];
 }
 
 - (void) cancel
 {
-  self.cancelled = YES;
-  [self.task terminate];
-  self.task = nil;
+	self.cancelled = YES;
+	[self.task terminate];
+	self.task = nil;
 }
 
 
@@ -78,84 +78,84 @@
 
 - (void) processCommits:(NSArray*) theCommits
 {
-  // TODO: put matching in the background queues
-  
-  BOOL gotNewCommits = NO;
-  
-  if (!self.searchCache)
-  {
-    self.searchCache = [NSMutableArray array];
-  }
-  if ([(NSArray*)self.searchCache count] < 300 && theCommits)
-  {
-    [self.searchCache addObjectsFromArray:theCommits];
-  }
-  
-  for (GBCommit* commit in theCommits)
-  {
-    if (lastTimestamp <= 0 || commit.rawTimestamp < self.lastTimestamp)
-    {
-      self.lastTimestamp = commit.rawTimestamp;
-      gotNewCommits = YES;
-    }
-    if (![self.commitIds containsObject:commit.commitId])
-    {
-      commit.searchQuery = self.query;
-      if ([commit matchesQuery])
-      {
-        [self.commitIds addObject:commit.commitId];
-        [self.commits addObject:commit];
-      }
-    }
-  }
-  
-  self.isRunning = gotNewCommits;
-  [self.target performSelector:self.action withObject:self];
-  
-  self.task = nil;
-  
-  if (gotNewCommits)
-  {
-    [self launchNextTask];
-  }
+	// TODO: put matching in the background queues
+	
+	BOOL gotNewCommits = NO;
+	
+	if (!self.searchCache)
+	{
+		self.searchCache = [NSMutableArray array];
+	}
+	if ([(NSArray*)self.searchCache count] < 300 && theCommits)
+	{
+		[self.searchCache addObjectsFromArray:theCommits];
+	}
+	
+	for (GBCommit* commit in theCommits)
+	{
+		if (lastTimestamp <= 0 || commit.rawTimestamp < self.lastTimestamp)
+		{
+			self.lastTimestamp = commit.rawTimestamp;
+			gotNewCommits = YES;
+		}
+		if (![self.commitIds containsObject:commit.commitId])
+		{
+			commit.searchQuery = self.query;
+			if ([commit matchesQuery])
+			{
+				[self.commitIds addObject:commit.commitId];
+				[self.commits addObject:commit];
+			}
+		}
+	}
+	
+	self.isRunning = gotNewCommits;
+	[self.target performSelector:self.action withObject:self];
+	
+	self.task = nil;
+	
+	if (gotNewCommits)
+	{
+		[self launchNextTask];
+	}
 }
 
 
 - (void) launchNextTask
 {
-  if (self.task) return;
-  if (self.cancelled) return;
-  
-  if (!self.usedCachedCommits)
-  {
-    self.usedCachedCommits = YES;
-    if (self.searchCache)
-    {
-      NSArray* cachedCommits = [[self.searchCache retain] autorelease];
-      self.searchCache = nil;
-      [self processCommits:cachedCommits];
-      return;
-    }
-  }
-  
-  self.task = [GBHistoryTask task];
-  self.isRunning = YES;
-  
-  self.task.includeDiff = YES;
-  self.task.repository = self.repository;
-  self.task.branch = self.repository.currentLocalRef;
-  if ([self.repository doesRefExist:self.repository.currentRemoteBranch])
-  {
-    self.task.joinedBranch = self.repository.currentRemoteBranch;
-  }
-  self.task.limit = self.limit;
-  self.limit += 50; // so that we start quickly, but avoid very frequent calls when looking deeper in the history.
-  self.limit = MIN(self.limit, 300);
-  self.task.beforeTimestamp = self.lastTimestamp;
-  
-  [self.repository launchTask:self.task withBlock:^{
-    [self processCommits:self.task.commits];
-  }];
+	if (self.task) return;
+	if (self.cancelled) return;
+	
+	if (!self.usedCachedCommits)
+	{
+		self.usedCachedCommits = YES;
+		if (self.searchCache)
+		{
+			NSArray* cachedCommits = [[self.searchCache retain] autorelease];
+			self.searchCache = nil;
+			[self processCommits:cachedCommits];
+			return;
+		}
+	}
+	
+	self.task = [GBHistoryTask task];
+	self.isRunning = YES;
+	
+	self.task.includeDiff = YES;
+	self.task.repository = self.repository;
+	self.task.branch = self.repository.currentLocalRef;
+	if ([self.repository doesRefExist:self.repository.currentRemoteBranch])
+	{
+		self.task.joinedBranch = self.repository.currentRemoteBranch;
+	}
+	self.task.limit = self.limit;
+	self.limit += 50; // so that we start quickly, but avoid very frequent calls when looking deeper in the history.
+	self.limit = MIN(self.limit, 300);
+	self.task.beforeTimestamp = self.lastTimestamp;
+	
+	[self.repository launchTask:self.task withBlock:^{
+		[self processCommits:self.task.commits];
+	}];
 }
 
 
--- Classes/GBSearchQuery.m
+++ Classes/GBSearchQuery.m
@@ -15,48 +15,48 @@ NSRange GBSearchQueryRangeForTokenInString(id token, NSString* string);
 
 - (void) dealloc
 {
-  [sourceString release]; sourceString = nil;
-  [tokens release]; tokens = nil;
-  [super dealloc];
+	[sourceString release]; sourceString = nil;
+	[tokens release]; tokens = nil;
+	[super dealloc];
 }
 
 + (GBSearchQuery*) queryWithString:(NSString*)str
 {
-  GBSearchQuery* q = [[[self alloc] init] autorelease];
-  q.sourceString = str;
-  return q;
+	GBSearchQuery* q = [[[self alloc] init] autorelease];
+	q.sourceString = str;
+	return q;
 }
 
 - (NSString*) description
 {
-  return [NSString stringWithFormat:@"<GBSearchQuery:%p %@>", self, self.sourceString];
+	return [NSString stringWithFormat:@"<GBSearchQuery:%p %@>", self, self.sourceString];
 }
 
 - (BOOL) matchTokens:(BOOL(^)(id token))block
 {
-  if (!self.tokens || [self.tokens count] <= 0) return NO;
-  
-  for (id token in self.tokens)
-  {
-    if (!block(token)) return NO;
-  }
-  return YES; // all tokens matched.
+	if (!self.tokens || [self.tokens count] <= 0) return NO;
+	
+	for (id token in self.tokens)
+	{
+		if (!block(token)) return NO;
+	}
+	return YES; // all tokens matched.
 }
 
 // Returns YES if string contains all of the tokens of the receiver.
 - (BOOL) matchesString:(NSString*)string
 {
-  if (!string) return NO;
-  return [self matchTokens:^(id token){
-    NSRange range = GBSearchQueryRangeForTokenInString(token, string);
-    return (BOOL)(range.length > 0);
-  }];
+	if (!string) return NO;
+	return [self matchTokens:^(id token){
+		NSRange range = GBSearchQueryRangeForTokenInString(token, string);
+		return (BOOL)(range.length > 0);
+	}];
 }
 
 // Returns a range of otken occurence in string.
 - (NSRange) rangeOfToken:(id)token inString:(NSString*)string
 {
-  return GBSearchQueryRangeForTokenInString(token, string);
+	return GBSearchQueryRangeForTokenInString(token, string);
 }
 
 
@@ -66,19 +66,19 @@ NSRange GBSearchQueryRangeForTokenInString(id token, NSString* string);
 
 - (void) setSourceString:(NSString *)str
 {
-  if (sourceString == str) return;
-  
-  [sourceString release];
-  sourceString = [[str stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] retain];
-  
-  self.tokens = [self tokensForString:sourceString];
+	if (sourceString == str) return;
+	
+	[sourceString release];
+	sourceString = [[str stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] retain];
+	
+	self.tokens = [self tokensForString:sourceString];
 }
 
 - (NSArray*) tokensForString:(NSString*)str
 {
-  // TODO: parse quoted sequences as a single token
-  // TODO: use GBSearchQueryToken object to wrap tokens and case sensitivity (quoted sequences should be case-sensitive, others - not)
-  return [str componentsSeparatedByString:@" "];
+	// TODO: parse quoted sequences as a single token
+	// TODO: use GBSearchQueryToken object to wrap tokens and case sensitivity (quoted sequences should be case-sensitive, others - not)
+	return [str componentsSeparatedByString:@" "];
 }
 
 
@@ -86,7 +86,7 @@ NSRange GBSearchQueryRangeForTokenInString(id token, NSString* string);
 
 NSRange GBSearchQueryRangeForTokenInString(id token, NSString* string)
 {
-  // TODO: change string token to GBSearchQueryToken object
-  if (!token || !string) return NSMakeRange(NSNotFound, 0);
-  return [string rangeOfString:token options:NSCaseInsensitiveSearch|NSDiacriticInsensitiveSearch];
+	// TODO: change string token to GBSearchQueryToken object
+	if (!token || !string) return NSMakeRange(NSNotFound, 0);
+	return [string rangeOfString:token options:NSCaseInsensitiveSearch|NSDiacriticInsensitiveSearch];
 }
