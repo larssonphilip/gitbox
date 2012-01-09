35f182b784a5ad6cf88d7308a6721c8c4c8c7d6c
Oleg Andreev <oleganza@gmail.com>
2012-01-09 06:24:31 +0100

    local+target refs are considered changed if either commitId or name has changed.

--- Classes/GBRepository.m
+++ Classes/GBRepository.m
@@ -633,19 +633,10 @@
 						self.currentRemoteBranch = self.currentLocalRef.configuredRemoteBranch;
 					}
 					
-					// isEqual checks name equality.
-					BOOL didChange = (!currentRef || ![self.currentLocalRef isEqual:currentRef]) ||
-									 (!targetRef || ![self.currentRemoteBranch isEqual:targetRef]);
-					
-					// Also check if commit id has changed.
-					if (!self.currentLocalRef.commitId || ![currentRef.commitId isEqualToString:self.currentLocalRef.commitId])
-					{
-						didChange = YES;
-					}
-					if (!self.currentRemoteBranch.commitId || ![targetRef.commitId isEqualToString:self.currentRemoteBranch.commitId])
-					{
-						didChange = YES;
-					}
+					BOOL didChange = !(OAAreEqual(currentRef.commitId, self.currentLocalRef.commitId) &&
+									   OAAreEqual(currentRef.name,     self.currentLocalRef.name) &&
+									   OAAreEqual(targetRef.commitId, self.currentRemoteBranch.commitId) &&
+									   OAAreEqual(targetRef.name,     self.currentRemoteBranch.name));
 					
 					if (aBlock) aBlock(didChange);
 				}];
