bb9e150b4859835d7180b40eb7826e78b3961830
Oleg Andreev <oleganza@gmail.com>
2012-01-30 22:28:11 +0100

    diff prefs

--- Classes/GBPreferencesDiffViewController.h
+++ Classes/GBPreferencesDiffViewController.h
@@ -4,4 +4,14 @@
 
 + (GBPreferencesDiffViewController*) controller;
 
+@property(assign) BOOL isFileMergeAvailable;
+@property(assign) BOOL isKaleidoscopeAvailable;
+@property(assign) BOOL isChangesAvailable;
+@property(assign) BOOL isTextWranglerAvailable;
+@property(assign) BOOL isBBEditAvailable;
+@property(assign) BOOL isAraxisAvailable;
+@property(assign) BOOL isDiffMergeAvailable;
+
+- (NSArray*) diffTools;
+
 @end
--- Classes/GBPreferencesDiffViewController.m
+++ Classes/GBPreferencesDiffViewController.m
@@ -1,11 +1,81 @@
+#import "OATask.h"
+#import "GBChange.h"
 #import "GBPreferencesDiffViewController.h"
 
-@implementation GBPreferencesDiffViewController
+@implementation GBPreferencesDiffViewController {
+	BOOL loadingDiffToolsStatus;
+}
 
 + (GBPreferencesDiffViewController*) controller
 {
 	return [[[self alloc] initWithNibName:@"GBPreferencesDiffViewController" bundle:nil] autorelease];
 }
+
+@synthesize isFileMergeAvailable;
+@synthesize isKaleidoscopeAvailable;
+@synthesize isChangesAvailable;
+@synthesize isTextWranglerAvailable;
+@synthesize isBBEditAvailable;
+@synthesize isAraxisAvailable;
+@synthesize isDiffMergeAvailable;
+
+- (void) dealloc
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+	[super dealloc];
+}
+
+- (NSArray*) diffTools
+{
+	return [GBChange diffTools];
+}
+
+- (void) loadDiffToolsStatus
+{
+	if (!loadingDiffToolsStatus)
+	{
+		loadingDiffToolsStatus = YES;
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+			BOOL fm = !![OATask systemPathForExecutable:@"opendiff"];
+			dispatch_async(dispatch_get_main_queue(), ^{
+				self.isFileMergeAvailable = fm;
+			});
+			BOOL ks = !![OATask systemPathForExecutable:@"ksdiff"];
+			dispatch_async(dispatch_get_main_queue(), ^{
+				self.isKaleidoscopeAvailable = ks;
+			});
+			BOOL ch = !![OATask systemPathForExecutable:@"chdiff"];
+			dispatch_async(dispatch_get_main_queue(), ^{
+				self.isChangesAvailable = ch;
+			});
+			BOOL tw = !![OATask systemPathForExecutable:@"twdiff"];
+			dispatch_async(dispatch_get_main_queue(), ^{
+				self.isTextWranglerAvailable = tw;
+			});
+			BOOL bb = !![OATask systemPathForExecutable:@"bbdiff"];
+			dispatch_async(dispatch_get_main_queue(), ^{
+				self.isBBEditAvailable = bb;
+			});
+			BOOL dm = !![OATask systemPathForExecutable:@"diffmerge"];
+			dispatch_async(dispatch_get_main_queue(), ^{
+				self.isDiffMergeAvailable = dm;
+			});
+			BOOL ax = !![OATask systemPathForExecutable:@"compare"] || !![OATask systemPathForExecutable:@"araxis"];
+			dispatch_async(dispatch_get_main_queue(), ^{
+				self.isAraxisAvailable = ax;
+			});
+			dispatch_async(dispatch_get_main_queue(), ^{
+				loadingDiffToolsStatus = NO;
+			});
+		});
+	}
+}
+
+- (void) windowDidBecomeKey:(NSNotification *)notification
+{
+	[self loadDiffToolsStatus];
+}
+
 
 
 #pragma mark - MASPreferencesViewController
@@ -24,6 +94,25 @@
 - (NSString *)toolbarItemLabel
 {
     return NSLocalizedString(@"Diff Tools", nil);
+}
+
+- (void) viewWillAppear
+{
+	[self loadDiffToolsStatus];
+	dispatch_async(dispatch_get_main_queue(), ^{
+		if (self.view.window)
+		{
+			[[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowDidBecomeKeyNotification object:nil];
+			[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidBecomeKey:) name:NSWindowDidBecomeKeyNotification object:self.view.window];
+			[self loadDiffToolsStatus];
+		}
+	});
+}
+
+- (NSView*)initialKeyView
+{
+	NSView* v = [self.view viewWithTag:1];
+	return v ? v : self.view;
 }
 
 @end
