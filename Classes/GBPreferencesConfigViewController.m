e639e55f84a73b2fd74e059c861c080663af7dc7
Oleg Andreev <oleganza@gmail.com>
2012-02-01 07:25:36 +0100

    gitconfig done

--- Classes/GBPreferencesConfigViewController.m
+++ Classes/GBPreferencesConfigViewController.m
@@ -18,6 +18,35 @@
 	return [[[self alloc] initWithNibName:@"GBPreferencesConfigViewController" bundle:nil] autorelease];
 }
 
+- (NSURL*) configURL
+{
+	return [NSURL fileURLWithPath:[NSHomeDirectory() stringByAppendingPathComponent:@".gitconfig"]];
+}
+
+- (void) loadConfigData
+{
+	NSData* configData = [NSData dataWithContentsOfURL:[self configURL]];
+	NSString* configString = [configData UTF8String];
+	
+	[self.configTextView setString:configString ? configString : @""];
+}
+
+- (void) loadBasicData
+{
+	id name = [[GBGitConfig userConfig] userName];
+	id email = [[GBGitConfig userConfig] userEmail];
+	
+	[self.nameTextField setStringValue:name ? name : @""];
+	[self.emailTextField setStringValue:email ? email : @""];
+}
+
+
+- (void) loadData
+{
+	[self loadBasicData];
+	[self loadConfigData];
+}
+
 
 - (IBAction) toggleMode:(id)sender
 {
@@ -29,29 +58,35 @@
 
 - (IBAction) nameOrEmailDidChange:(id)sender
 {
-	
+	static int counter = 0;
+	counter++;
+	int c = counter;
+	double delayInSeconds = 1.0;
+	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+	dispatch_after(popTime, dispatch_get_main_queue(), ^{
+		if (c != counter) return;
+		[[GBGitConfig userConfig] setName:self.nameTextField.stringValue 
+									email:self.emailTextField.stringValue 
+								withBlock:^{
+									[self loadConfigData];
+		}];
+	});
 }
 
 - (void) textDidChange:(NSNotification*)notification
 {
-	
+	static int counter = 0;
+	counter++;
+	int c = counter;
+	double delayInSeconds = 1.0;
+	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+	dispatch_after(popTime, dispatch_get_main_queue(), ^{
+		if (c != counter) return;
+		[[self.configTextView.string dataUsingEncoding:NSUTF8StringEncoding] writeToURL:[self configURL] atomically:YES];
+		[self loadBasicData];
+	});
 }
 
-- (void) loadData
-{
-	// Load ~/.gitconfig contents and name/email pair.
-	
-	id name = [[GBGitConfig userConfig] userName];
-	id email = [[GBGitConfig userConfig] userEmail];
-	
-	[self.nameTextField setStringValue:name ? name : @""];
-	[self.emailTextField setStringValue:email ? email : @""];
-	
-	NSData* configData = [NSData dataWithContentsOfURL:[NSURL fileURLWithPath:[NSHomeDirectory() stringByAppendingPathComponent:@".gitconfig"]]];
-	NSString* configString = [configData UTF8String];
-	
-	[self.configTextView setString:configString ? configString : @""];
-}
 
 - (void) loadView
 {
