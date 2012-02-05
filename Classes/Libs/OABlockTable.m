02d85ae43ada2067833af2bf743f4b0f2aceaaad
Oleg Andreev <oleganza@gmail.com>
2012-02-05 23:15:18 +0100

    debugging OABlockTable

--- Classes/Libs/OABlockTable.m
+++ Classes/Libs/OABlockTable.m
@@ -3,62 +3,70 @@
 
 @interface OABlockTable ()
 @property(nonatomic, retain) NSMutableDictionary* table;
+@property(nonatomic, retain) NSMutableDictionary* tableCounters;
 @end
 
 @implementation OABlockTable
 
 @synthesize table;
+@synthesize tableCounters;
 
 - (id)init
 {
-  if ((self = [super init]))
-  {
-    self.table = [NSMutableDictionary dictionary];
-  }
-  return self;
+	if ((self = [super init]))
+	{
+		self.table = [NSMutableDictionary dictionary];
+		self.tableCounters = [NSMutableDictionary dictionary];
+	}
+	return self;
 }
 
 - (void)dealloc
 {
-  self.table = nil;
-  [super dealloc];
+	self.table = nil;
+	self.tableCounters = nil;
+	[super dealloc];
 }
 
 - (BOOL) containsBlockForName:(NSString*)aName
 {
-  return !![self.table objectForKey:aName];
+	return !![self.table objectForKey:aName];
 }
 
 - (void) addBlock:(void(^)())aBlock forName:(NSString*)aName
 {
-  if (!aBlock) aBlock = ^{}; // put an empty block to mark associated task as running
-  [self.table setObject:OABlockConcat([self.table objectForKey:aName], aBlock) forKey:aName];
+	if (!aBlock) aBlock = ^{}; // put an empty block to mark associated task as running
+	[self.table setObject:OABlockConcat([self.table objectForKey:aName], aBlock) forKey:aName];
+	int c = [[self.tableCounters objectForKey:aName] intValue];
+	c++;
+	[self.tableCounters setObject:[NSNumber numberWithInt:c] forKey:aName];
 }
 
 - (void) callBlockForName:(NSString*)aName
 {
-  void(^aBlock)() = [[self.table objectForKey:aName] retain];
-  [self.table removeObjectForKey:aName]; // it is important to remove the block before it is called
-  if (aBlock) aBlock();
-  [aBlock release];
+	void(^aBlock)() = [[self.table objectForKey:aName] retain];
+	[self.table removeObjectForKey:aName]; // it is important to remove the block before it is called
+	[self.tableCounters removeObjectForKey:aName];
+	if (aBlock) aBlock();
+	[aBlock release];
 }
 
 - (void) addBlock:(void(^)())aBlock forName:(NSString*)aName proceedIfClear:(void(^)())continuation
 {
-  if ([self containsBlockForName:aName])
-  {
-    [self addBlock:aBlock forName:aName];
-  }
-  else
-  {
-    [self addBlock:aBlock forName:aName];
-    if (continuation) continuation();
-  }  
+	if ([self containsBlockForName:aName])
+	{
+		[self addBlock:aBlock forName:aName];
+	}
+	else
+	{
+		[self addBlock:aBlock forName:aName];
+		if (continuation) continuation();
+	}  
 }
 
 - (NSString*) description
 {
-  return [NSString stringWithFormat:@"<OABlockTable:%p names: %@>", [self.table allKeys]];
+	return [NSString stringWithFormat:@"<OABlockTable:%p blocks: %@>", self, self.tableCounters];
 }
 
 
@@ -67,7 +75,7 @@
 // Returns a global shared blocktable.
 + (OABlockTable*) sharedTable
 {
-  static OABlockTable* volatile OABlockTableSharedInstance = nil;
+	static OABlockTable* volatile OABlockTableSharedInstance = nil;
 	static dispatch_once_t OABlockTableSharedInstanceOnce = 0;
 	
 	dispatch_once( &OABlockTableSharedInstanceOnce, ^{ OABlockTableSharedInstance = [[self alloc] init]; });
@@ -76,27 +84,27 @@
 
 + (BOOL) containsBlockForName:(NSString*)aName
 {
-  return [[self sharedTable] containsBlockForName:aName];
+	return [[self sharedTable] containsBlockForName:aName];
 }
 
 + (void) addBlock:(void(^)())aBlock forName:(NSString*)aName
 {
-  return [[self sharedTable] addBlock:aBlock forName:aName];
+	return [[self sharedTable] addBlock:aBlock forName:aName];
 }
 
 + (void) callBlockForName:(NSString*)aName
 {
-  return [[self sharedTable] callBlockForName:aName];
+	return [[self sharedTable] callBlockForName:aName];
 }
 
 + (void) addBlock:(void(^)())aBlock forName:(NSString*)aName proceedIfClear:(void(^)())continuation
 {
-  return [[self sharedTable] addBlock:aBlock forName:aName proceedIfClear:continuation];  
+	return [[self sharedTable] addBlock:aBlock forName:aName proceedIfClear:continuation];  
 }
 
 + (NSString*) description
 {
-  return [NSString stringWithFormat:@"<OABlockTable:%p (shared) names: %@>", [[self sharedTable].table allKeys]];
+	return [NSString stringWithFormat:@"<OABlockTable:%p (shared) names: %@>", [[self sharedTable].table allKeys]];
 }
 
 @end
