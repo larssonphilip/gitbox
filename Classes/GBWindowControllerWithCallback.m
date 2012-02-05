8666a1930ee89c8c113fc85614e672a6393243c9
Oleg Andreev <oleganza@gmail.com>
2012-02-05 15:15:17 +0100

    indent

--- Classes/GBWindowControllerWithCallback.m
+++ Classes/GBWindowControllerWithCallback.m
@@ -7,25 +7,25 @@
 
 - (void) dealloc
 {
-  [completionHandler release]; completionHandler = nil;
-  [super dealloc];
+	[completionHandler release]; completionHandler = nil;
+	[super dealloc];
 }
 
 - (void) performCompletionHandler:(BOOL)cancelled
 {
-  if (self.completionHandler) self.completionHandler(cancelled);
-  self.completionHandler = nil;
+	if (self.completionHandler) self.completionHandler(cancelled);
+	self.completionHandler = nil;
 }
 
 - (void) presentSheetInMainWindow
 {
-  void(^block)(BOOL) = self.completionHandler;
-  
-  self.completionHandler = ^(BOOL cancelled){
-    if (block) block(cancelled);
-    [[GBMainWindowController instance] dismissSheet:self];
-  };
-  [[GBMainWindowController instance] presentSheet:self];
+	void(^block)(BOOL) = self.completionHandler;
+	
+	self.completionHandler = ^(BOOL cancelled){
+		if (block) block(cancelled);
+		[[GBMainWindowController instance] dismissSheet:self];
+	};
+	[[GBMainWindowController instance] presentSheet:self];
 }
 
 @end
