bbdff9b8f9c7d3c20e551fffe4092a9b16fbe6ed
Oleg Andreev <oleganza@gmail.com>
2012-02-10 07:16:32 +0100

    fixed one more bug

--- Classes/GBPromptController.m
+++ Classes/GBPromptController.m
@@ -1,7 +1,9 @@
 #import "GBPromptController.h"
 #import "NSString+OAStringHelpers.h"
 
-@implementation GBPromptController
+@implementation GBPromptController {
+	BOOL didInitField;
+}
 
 @synthesize textField;
 
@@ -17,83 +19,87 @@
 
 + (GBPromptController*) controller
 {
-  return [[[self alloc] initWithWindowNibName:@"GBPromptController"] autorelease];
+	return [[[self alloc] initWithWindowNibName:@"GBPromptController"] autorelease];
 }
 
 - (id) initWithWindow:(NSWindow*)window
 {
-  if ((self = [super initWithWindow:window]))
-  {
-    self.value = @"";
-    self.title = NSLocalizedString(@"Prompt",@"");
-    self.promptText = NSLocalizedString(@"Prompt:",@"");
-    self.buttonText = NSLocalizedString(@"OK",@"");
-    self.requireNonNilValue = YES;
-    self.requireNonEmptyString = YES;
-  }
-  return self;
+	if ((self = [super initWithWindow:window]))
+	{
+		self.value = @"";
+		self.title = NSLocalizedString(@"Prompt",@"");
+		self.promptText = NSLocalizedString(@"Prompt:",@"");
+		self.buttonText = NSLocalizedString(@"OK",@"");
+		self.requireNonNilValue = YES;
+		self.requireNonEmptyString = YES;
+	}
+	return self;
 }
 
 - (void) dealloc
 {
-  [NSObject cancelPreviousPerformRequestsWithTarget:self];
-  self.textField = nil;
-  self.title = nil;
-  self.promptText = nil;
-  self.buttonText = nil;
-  self.value = nil;
-  [super dealloc];
+	[NSObject cancelPreviousPerformRequestsWithTarget:self];
+	self.textField = nil;
+	self.title = nil;
+	self.promptText = nil;
+	self.buttonText = nil;
+	self.value = nil;
+	[super dealloc];
 }
 
 - (IBAction) onOK:(id)sender
 {
-  self.value = [self.textField stringValue];
-  if (requireSingleLine || requireStripWhitespace)
-  {
-    self.value = [self.value stringByReplacingOccurrencesOfString:@"\n" withString:@""];
-  }
-  if (requireStripWhitespace)
-  {
-    self.value = [self.value stringByReplacingOccurrencesOfString:@"\t" withString:@""];
-    self.value = [self.value stringByReplacingOccurrencesOfString:@" " withString:@""];
-  }
-  if (requireNonNilValue && !self.value) self.value = @"";
-  if (requireNonEmptyString && (!self.value || [self.value isEmptyString])) return;
-  
-  [[self retain] autorelease];
-  [self performCompletionHandler:NO];
-  self.value = @"";
+	self.value = [self.textField stringValue];
+	if (requireSingleLine || requireStripWhitespace)
+	{
+		self.value = [self.value stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+	}
+	if (requireStripWhitespace)
+	{
+		self.value = [self.value stringByReplacingOccurrencesOfString:@"\t" withString:@""];
+		self.value = [self.value stringByReplacingOccurrencesOfString:@" " withString:@""];
+	}
+	if (requireNonNilValue && !self.value) self.value = @"";
+	if (requireNonEmptyString && (!self.value || [self.value isEmptyString])) return;
+	
+	[[self retain] autorelease];
+	[self performCompletionHandler:NO];
+	self.value = @"";
 }
 
 - (IBAction) onCancel:(id)sender
 {
-  [self performCompletionHandler:YES];
+	[self performCompletionHandler:YES];
 }
 
 - (void)windowDidBecomeKey:(NSNotification *)notification
 {
-  if (self.requireSingleLine)
-  {
-    NSWindow* win = [self window];
-    NSSize minSize = [win contentMinSize];
-    NSSize maxSize = [win contentMaxSize];
-    maxSize.height = minSize.height;
-    [win setContentMaxSize:maxSize];
-    
-    // Note: when the field is wrapping in NIB, these options do not help. 
-    // So we set NIB to have single-line scrolling textfield and override it in multiline mode.
-//    [[self.textField cell] setLineBreakMode:NSLineBreakByClipping];
-//    [[self.textField cell] setUsesSingleLineMode:NO];
-//    [[self.textField cell] setWraps:NO];
-//    [[self.textField cell] setScrollable:YES];
-  }
-  else
-  {
-    [[self.textField cell] setLineBreakMode:NSLineBreakByWordWrapping];
-    [[self.textField cell] setWraps:YES];
-  }
-
-  [self.textField setStringValue:self.value ? self.value : @""];
+	if (self.requireSingleLine)
+	{
+		NSWindow* win = [self window];
+		NSSize minSize = [win contentMinSize];
+		NSSize maxSize = [win contentMaxSize];
+		maxSize.height = minSize.height;
+		[win setContentMaxSize:maxSize];
+		
+		// Note: when the field is wrapping in NIB, these options do not help. 
+		// So we set NIB to have single-line scrolling textfield and override it in multiline mode.
+		//    [[self.textField cell] setLineBreakMode:NSLineBreakByClipping];
+		//    [[self.textField cell] setUsesSingleLineMode:NO];
+		//    [[self.textField cell] setWraps:NO];
+		//    [[self.textField cell] setScrollable:YES];
+	}
+	else
+	{
+		[[self.textField cell] setLineBreakMode:NSLineBreakByWordWrapping];
+		[[self.textField cell] setWraps:YES];
+	}
+	
+	if (!didInitField)
+	{
+		didInitField = YES;
+		[self.textField setStringValue:self.value ? self.value : @""];
+	}
 }
 
 @end
