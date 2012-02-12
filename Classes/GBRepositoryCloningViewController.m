70f3d40bc6cce5711754bde18b57318500fa8d5a
Oleg Andreev <oleganza@gmail.com>
2012-02-12 18:23:27 +0100

    fixed ui when cloning fails

--- Classes/GBRepositoryCloningViewController.m
+++ Classes/GBRepositoryCloningViewController.m
@@ -16,56 +16,56 @@
 
 - (void) dealloc
 {
-  [[NSNotificationCenter defaultCenter] removeObserver:self];
-  self.messageLabel = nil;
-  self.errorLabel = nil;
-  self.cancelButton = nil;
-  self.progressIndicator = nil;
-  [super dealloc];
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+	self.messageLabel = nil;
+	self.errorLabel = nil;
+	self.cancelButton = nil;
+	self.progressIndicator = nil;
+	[super dealloc];
 }
 
 - (void) setRepositoryController:(GBRepositoryCloningController*)repoCtrl
 {
-  if (repositoryController == repoCtrl) return;
-  
-  [repositoryController removeObserverForAllSelectors:self];
-  
-  repositoryController = repoCtrl;
-  
-  [repositoryController addObserverForAllSelectors:self];
-  
-  [self view]; // load view
-  [self update];
+	if (repositoryController == repoCtrl) return;
+	
+	[repositoryController removeObserverForAllSelectors:self];
+	
+	repositoryController = repoCtrl;
+	
+	[repositoryController addObserverForAllSelectors:self];
+	
+	[self view]; // load view
+	[self update];
 }
 
 
 - (void) loadView
 {
-  [super loadView];
-  [self update];
-  [self.progressIndicator setIndeterminate:YES];
-  [self.progressIndicator startAnimation:nil];
+	[super loadView];
+	[self update];
+	[self.progressIndicator setIndeterminate:YES];
+	[self.progressIndicator startAnimation:nil];
 }
 
 - (IBAction) cancel:(id)sender
 {
-  [self.repositoryController cancelCloning];
+	[self.repositoryController cancelCloning];
 }
 
 - (void) update
 {
-  NSURL* URL = self.repositoryController.sourceURL;
-  [self.messageLabel setStringValue:[NSString stringWithFormat:NSLocalizedString(@"Cloning %@", @"Clone"), URL ? [URL absoluteString] : @""]];
-
-  [self.errorLabel setStringValue:@""];
-  [self.cancelButton setTitle:NSLocalizedString(@"Cancel", @"")];
-  
-  if (self.repositoryController.error)
-  {
-    [self.messageLabel setStringValue:NSLocalizedString(@"Clone Error", @"Clone")];
-    [self.errorLabel setStringValue:NSLocalizedString(@"Check the URL or the network connection.", @"Clone")];
-    [self.cancelButton setTitle:NSLocalizedString(@"Close", @"")];
-  }
+	NSURL* URL = self.repositoryController.sourceURL;
+	[self.messageLabel setStringValue:[NSString stringWithFormat:NSLocalizedString(@"Cloning %@", @"Clone"), URL ? [URL absoluteString] : @""]];
+	
+	[self.errorLabel setStringValue:@""];
+	[self.cancelButton setTitle:NSLocalizedString(@"Cancel", @"")];
+	
+	if (self.repositoryController.error)
+	{
+		[self.messageLabel setStringValue:NSLocalizedString(@"Clone Error", @"Clone")];
+		[self.errorLabel setStringValue:NSLocalizedString(@"Check the URL or the network connection.", @"Clone")];
+		[self.cancelButton setTitle:NSLocalizedString(@"Close", @"")];
+	}
 }
 
 
@@ -77,25 +77,28 @@
 
 - (void) cloningRepositoryControllerDidStart:(GBRepositoryCloningController*)ctrl
 {
-  [self update];
-  [self.errorLabel setStringValue:NSLocalizedString(@"Preparing...", @"Clone")];
+	[self update];
+	[self.errorLabel setStringValue:NSLocalizedString(@"Preparing...", @"Clone")];
 }
 
 
 - (void) cloningRepositoryControllerProgress:(GBRepositoryCloningController*)ctrl
 {
-  double pr = ctrl.sidebarItemProgress;
-  [self.progressIndicator setIndeterminate:pr < 0.02];
-  [self.progressIndicator setDoubleValue:pr];
-  [self.errorLabel setStringValue:ctrl.progressStatus ? ctrl.progressStatus : @""];
+	double pr = ctrl.sidebarItemProgress;
+	[self.progressIndicator setIndeterminate:pr < 0.02];
+	[self.progressIndicator setDoubleValue:pr];
+	[self.errorLabel setStringValue:ctrl.progressStatus ? ctrl.progressStatus : @""];
 }
 
 
 - (void) cloningRepositoryControllerDidFail:(GBRepositoryCloningController*)cloningRepoCtrl
 {
-  [self.messageLabel setStringValue:NSLocalizedString(@"Clone Error", @"Clone")];
-  [self.errorLabel setStringValue:NSLocalizedString(@"Check the URL or the network connection.", @"Clone")];
-  [self.cancelButton setTitle:NSLocalizedString(@"Close", @"")];
+	[self.messageLabel setStringValue:NSLocalizedString(@"Clone Error", @"Clone")];
+	[self.errorLabel setStringValue:NSLocalizedString(@"Check the URL or the network connection.", @"Clone")];
+	[self.cancelButton setTitle:NSLocalizedString(@"Close", @"")];
+	[self.progressIndicator setIndeterminate:NO];
+	[self.progressIndicator stopAnimation:nil];
+	[self.progressIndicator setDoubleValue:0.0];
 }
 
 - (void) cloningRepositoryControllerDidCancel:(GBRepositoryCloningController*)cloningRepoCtrl
@@ -104,8 +107,8 @@
 
 - (void) cloningRepositoryControllerDidFinish:(GBRepositoryCloningController*)cloningRepoCtrl
 {
-  [self.messageLabel setStringValue:NSLocalizedString(@"Clone Finished", @"Clone")];
-  [self.cancelButton setTitle:NSLocalizedString(@"Close", @"")];
+	[self.messageLabel setStringValue:NSLocalizedString(@"Clone Finished", @"Clone")];
+	[self.cancelButton setTitle:NSLocalizedString(@"Close", @"")];
 }
 
 
